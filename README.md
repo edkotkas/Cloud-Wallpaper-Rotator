df5b96103f2b8d8fac3e49d5ae72c6ee1be56a7f
edkotkas <edkotkas@gmail.com>
2016-05-05 03:27:29 +0000

    minor corrections

--- README.md
+++ README.md
@@ -1,35 +1,44 @@
 # Cloud Wallpaper Rotator (CWR)
-### Google Drive Only (for now)!
+
+## Google Drive Only (for now)!
 
 CWR, downloads a single wallpaper from a designated Google Drive folder randomly and then through the use of scripts can set the Wallpaper on your desktop.
 
-### Version
+## Version
+
 0.1
 
-### Installation
+## Installation
+
 To use this w/ Google Drive, you need the following:
-  - A client_secrets.json file (https://developers.google.com/drive/v2/web/auth/web-server).
-  - PyDrive module.
-  - Python 2.7.x
+
+- A client_secrets.json file (<https://developers.google.com/drive/v2/web/auth/web-server>).
+- PyDrive module.
+- Python 2.7.x
 
 Install PyDrive for Python 2:
+
 ```sh
 $ pip2 install PyDrive
 ```
+
 Set up a google developer account, and acquire the client secrets file. Place this in the root of the folder with the .py script and name it "client_secrets.json".
 
 Change the "settings.json" file to your liking. History is a boolean for on and off, it will keep track of the last wallpaper that was used. OrderBy is the sorting order of the folder (doesn't really affect it, since it's randomly selected) more on it can be found [here](https://developers.google.com/drive/v2/reference/files/list#parameters). CacheAge is in days and it will update after that many days has passed since last cache update. The script only stores the new wallpaper id's from gdrive when the cache reaches this.
+
 ```json
 {
-	"folderId": "enter your GDrive folder ID here",
-	"history": 1,
-	"orderBy": "recency",
-	"cacheAge": 2
+    "folderId": "enter your GDrive folder ID here",
+    "history": 1,
+    "orderBy": "recency",
+    "cacheAge": 2
 }
 ```
 
-### Scripts
+## Scripts
+
 Sample script to enable use on Gnome-based Desktop Environments. Make it executable and run it after the startup of the DE.
+
 ```sh
 #!/bin/bash
 
@@ -47,22 +56,23 @@ interval="900" # frequency of wallpaper changes (in seconds)
 
 # change "set/path/to" to corresponding paths of the respective files
 while true; do
-	python2 set/path/to/cwr.py 1 0
-	gsettings set org.gnome.desktop.background picture-uri file://set/path/to/wallpaper.png
-	echo "Sleeping $interval seconds till the next change..."
-	sleep $interval
+    python2 set/path/to/cwr.py 1 0
+    gsettings set org.gnome.desktop.background picture-uri file://set/path/to/wallpaper.png
+    echo "Sleeping $interval seconds till the next change..."
+    sleep $interval
 done
-
 ```
+
 Sample of Windows script to change the wallpaper. (UNTESTED!)
+
 ```batch
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d c:\set\path\to\wallpaper.png
 ```
 
-### Todos
+## Todos
 
- - Write more detailed README.
- - Improve code.
- - Add Code Comments.
- - UI?
- - Support for other Cloud Storage Services.
+- Write more detailed README.
+- Improve code.
+- Add Code Comments.
+- UI?
+- Support for other Cloud Storage Services.
--- cwr.py
+++ cwr.py
@@ -5,136 +5,137 @@ import json
 import random
 import time
 
+
 class History(object):
 
-	def __init__(self, settings):
-		self.settings = settings
-		self.history_file = None
+    def __init__(self, settings):
+        self.settings = settings
+        self.history_file = None
 
-	def setHistoryFile(self, history_file):
-		self.history_file = history_file
+    def setHistoryFile(self, history_file):
+        self.history_file = history_file
 
-	def preserve(self, fileId):
-		if self.status() is True:
-			with open(self.history_file, "a") as history_file:
-				history_file.write(fileId+"\n")
+    def preserve(self, fileId):
+        if self.status() is True:
+            with open(self.history_file, "a") as history_file:
+                history_file.write(fileId + "\n")
 
-	def clear(self):
-		with open(self.history_file, "w") as history_file:
-			history_file.write("")
+    def clear(self):
+        with open(self.history_file, "w") as history_file:
+            history_file.write("")
 
-	def retrieve(self):
-		with open(self.history_file, "r") as history:
-			return [str(imageId).strip() for imageId in history.readlines()]
+    def retrieve(self):
+        with open(self.history_file, "r") as history:
+            return [str(imageId).strip() for imageId in history.readlines()]
 
-	def status(self):
-		return bool(self.settings["history"])
+    def status(self):
+        return bool(self.settings["history"])
 
-	def disable(self):
-		if self.status():
-			self.settings["history"] = 0
-			self.clear()
-		print("Already off.")
+    def disable(self):
+        if self.status():
+            self.settings["history"] = 0
+            self.clear()
+        print("Already off.")
 
-	def enable(self):
-		if not self.status():
-			self.settings["history"] = 1
-		print("Already on.")
+    def enable(self):
+        if not self.status():
+            self.settings["history"] = 1
+        print("Already on.")
+
 
 class Cache(object):
 
-	def __init__(self, cache_file):
-		self.cache_file = cache_file
-		self.cache_age = int(time.time() - os.path.getmtime(self.cache_file))
+    def __init__(self, cache_file):
+        self.cache_file = cache_file
+        self.cache_age = int(time.time() - os.path.getmtime(self.cache_file))
 
-	def preserve(self, imageList):
-		with open(self.cache_file, "w+") as cache:
-			cache.writelines(i+"\n" for i in imageList)
+    def preserve(self, imageList):
+        with open(self.cache_file, "w+") as cache:
+            cache.writelines(i + "\n" for i in imageList)
 
-	def retrieveList(self):
-		with open(self.cache_file, "r") as cache:
-			return [str(imageId).strip() for imageId in cache.readlines()]
+    def retrieveList(self):
+        with open(self.cache_file, "r") as cache:
+            return [str(imageId).strip() for imageId in cache.readlines()]
 
 
 class Background(object):
 
-	def __init__(self):
-		self.realpath = "/".join(
-			str(os.path.realpath(__file__)).split("/")[:-1]
-		) + "/"
+    def __init__(self):
+        self.realpath = "/".join(
+            str(os.path.realpath(__file__)).split("/")[:-1]
+        ) + "/"
 
-		self.output = self.realpath+"wallpaper.png"
-		self.wallpaper = None
+        self.output = self.realpath + "wallpaper.png"
+        self.wallpaper = None
 
-		with open(self.realpath+"settings.json", "r") as settings:
-			self.settings = json.load(settings)
+        with open(self.realpath + "settings.json", "r") as settings:
+            self.settings = json.load(settings)
 
-		self.cache = Cache(self.realpath+".cache")
+        self.cache = Cache(self.realpath + ".cache")
 
-		self.history = History(self.settings)
-		if self.history.status() is True:
-			self.history.setHistoryFile(".history")
+        self.history = History(self.settings)
+        if self.history.status() is True:
+            self.history.setHistoryFile(".history")
 
-		self.folderId = str(self.settings["folderId"])
-		self.orderBy = str(self.settings["orderBy"])
-		self.cacheAge = ((int(self.settings["cacheAge"])*24)*60)*60
+        self.folderId = str(self.settings["folderId"])
+        self.orderBy = str(self.settings["orderBy"])
+        self.cacheAge = ((int(self.settings["cacheAge"]) * 24) * 60) * 60
 
+        self.credentials = self.realpath + ".credentials"
 
-		self.credentials = self.realpath+".credentials"
+        self.gauth = GoogleAuth()
+        self.gauth.LoadCredentialsFile(self.credentials)
 
-		self.gauth = GoogleAuth()
-		self.gauth.LoadCredentialsFile(self.credentials)
+        if self.gauth.credentials is None:
+            self.gauth.LocalWebserverAuth()
+        elif self.gauth.access_token_expired:
+            self.gauth.Refresh()
+        else:
+            self.gauth.Authorize()
+        self.gauth.SaveCredentialsFile(self.credentials)
 
-		if self.gauth.credentials is None:
-		    self.gauth.LocalWebserverAuth()
-		elif self.gauth.access_token_expired:
-		    self.gauth.Refresh()
-		else:
-		    self.gauth.Authorize()
-		self.gauth.SaveCredentialsFile(self.credentials)
+        self.drive = GoogleDrive(self.gauth)
 
-		self.drive = GoogleDrive(self.gauth)
+        # writing back to settings file
+        # with open(self.realpath+"/settings.json", "w+") as settings:
+        # 	self.settings = json.dump(self.settings, settings)
 
-		# writing back to settings file
-		# with open(self.realpath+"/settings.json", "w+") as settings:
-		# 	self.settings = json.dump(self.settings, settings)
+    def cacheFileList(self, force=False):
+        if self.cache.cache_age >= self.cacheAge or force is True:
+            self.drive_files = self.drive.ListFile({
+                'orderBy': self.orderBy,
+                'q': "'" +
+                self.folderId +
+                "' in parents and trashed=false"
+            }).GetList()
+            print("Caching wallpaper list...")
+            self.cache.preserve([files['id'] for files in self.drive_files])
 
-	def cacheFileList(self, force=False):
-		if self.cache.cache_age >= self.cacheAge or force is True:
-			self.drive_files = self.drive.ListFile({
-				'orderBy': self.orderBy,
-				'q': "'" + \
-				self.folderId + \
-				"' in parents and trashed=false"
-			}).GetList()
-			print("Caching wallpaper list...")
-			self.cache.preserve([files['id'] for files in self.drive_files])
+    def retrieveFile(self, imageId):
+        print("Downloading wallpaper...")
+        self.drive.CreateFile({'id': imageId}).GetContentFile(self.output)
 
-	def retrieveFile(self, imageId):
-		print("Downloading wallpaper...")
-		self.drive.CreateFile({'id': imageId}).GetContentFile(self.output)
+    def nextWallpaper(self):
+        print("Retrieving next wallpaper...")
+        self.wallpaper = random.choice(self.cache.retrieveList())
+        if self.history.status() is True:
+            self.history.preserve(self.wallpaper)
+        self.retrieveFile(self.wallpaper)
 
-	def nextWallpaper(self):
-		print("Retrieving next wallpaper...")
-		self.wallpaper = random.choice(self.cache.retrieveList())
-		if self.history.status() is True:
-			self.history.preserve(self.wallpaper)
-		self.retrieveFile(self.wallpaper)
-
-	def previousWallpaper(self):
-		print("Retrieving previous wallpaper...")
-		if self.history.status() is True:
-			imageList = self.history.retrieve()
-			self.retrieveFile(imageList[-2])
+    def previousWallpaper(self):
+        print("Retrieving previous wallpaper...")
+        if self.history.status() is True:
+            imageList = self.history.retrieve()
+            self.retrieveFile(imageList[-2])
 
 if __name__ == '__main__':
-	import sys
-	try:
-		x, cache = int(sys.argv[1]), bool(int(sys.argv[2]))
-		Background().cacheFileList(cache)
-		if x == 0:
-			Background().previousWallpaper()
-		if x == 1:
-			Background().nextWallpaper()
-	except:
-		raise AttributeError("Invalid arguments.")
+    import sys
+    try:
+        x, cache = int(sys.argv[1]), bool(int(sys.argv[2]))
+        Background().cacheFileList(cache)
+        if x == 0:
+            Background().previousWallpaper()
+        if x == 1:
+            Background().nextWallpaper()
+    except:
+        raise AttributeError("Invalid arguments.")
